6bd37febd2cf6d916ab715e17424b05b16a63fea
Dorothy <dorothychauu@gmail.com>
2022-12-16 16:38:55 -0500

    commit tree 5c069cb04a6f875fe40cc8e22c5cc39b7cea032b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input#searchbyID');
+
+    console.log(input.value);
+
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+  });
 }
 
+
+
 document.addEventListener('DOMContentLoaded', init);
+// event.target.children[1].value
